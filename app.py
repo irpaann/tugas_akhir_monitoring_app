717c456b6f132e9f675033fdc953623087043edf
unknown <sigitaryadi150503@gmail.com>
2026-01-07 22:56:01 +0800

    Update dashboard payload tabel

--- app.py
+++ app.py
@@ -22,30 +22,103 @@ def dashboard():
 @app.route("/api/logs")
 def api_logs():
     db = get_db()
-    rows = db.execute("""
-        SELECT id, timestamp, ip, method, path,full_url, status, user_agent, payload_preview, threat_score
-        FROM logs
-        ORDER BY id DESC
-        LIMIT 20
-    """).fetchall()
 
-    logs = [
-        {
-            "id": r["id"],
-            "timestamp": r["timestamp"],
-            "ip": r["ip"],
-            "method": r["method"],
-            "path": r["path"],
-            "full_url": r["full_url"],
-            "status": r["status"],
-            "user_agent": r["user_agent"],
-            "payload": r["payload_preview"],
-            "threat_score": r["threat_score"]
-        }
-        for r in rows
-    ]
+    filters = []
+    params = []
+
+
+    start = request.args.get("start")
+    if start:
+        filters.append("timestamp >= ?")
+        params.append(start.replace("T", " ") + ":00")
+
+    end = request.args.get("end")
+    if end:
+        filters.append("timestamp <= ?")
+        params.append(end.replace("T", " ") + ":59")
+
+
+    ip = request.args.get("ip")
+    if ip:
+        filters.append("ip LIKE ?")
+        params.append(f"%{ip}%")
+
+    method = request.args.get("method")
+    if method:
+        filters.append("method = ?")
+        params.append(method)
+
+    status = request.args.get("status")
+    if status:
+        filters.append("status = ?")
+        params.append(status)
+
+    where_clause = ""
+    if filters:
+        where_clause = "WHERE " + " AND ".join(filters)
+
+    query = f"""
+        SELECT id, timestamp, ip, method, path, full_url,
+               status, user_agent, payload_preview, threat_score
+        FROM logs
+        {where_clause}
+        ORDER BY timestamp  DESC
+        LIMIT 100
+    """
+
+    rows = db.execute(query, params).fetchall()
+
+    logs = [{
+        "id": r["id"],
+        "timestamp": r["timestamp"],
+        "ip": r["ip"],
+        "method": r["method"],
+        "path": r["path"],
+        "full_url": r["full_url"],
+        "status": r["status"],
+        "user_agent": r["user_agent"],
+        "payload": r["payload_preview"],
+        "threat_score": r["threat_score"]
+    } for r in rows]
 
     return jsonify({"logs": logs})
+
+
+
+def build_filters(request):
+    filters = []
+    params = []
+
+    start = request.args.get("start")
+    if start:
+        filters.append("timestamp >= ?")
+        params.append(start.replace("T", " ") + ":00")
+
+    end = request.args.get("end")
+    if end:
+        filters.append("timestamp <= ?")
+        params.append(end.replace("T", " ") + ":59")
+
+    ip = request.args.get("ip")
+    if ip:
+        filters.append("ip LIKE ?")
+        params.append(f"%{ip}%")
+
+    method = request.args.get("method")
+    if method:
+        filters.append("method = ?")
+        params.append(method)
+
+    status = request.args.get("status")
+    if status:
+        filters.append("status = ?")
+        params.append(status)
+
+    where_clause = ""
+    if filters:
+        where_clause = "WHERE " + " AND ".join(filters)
+
+    return where_clause, params
 
 
 # ============================
@@ -73,7 +146,7 @@ def receive_log():
         db.execute("""
             INSERT INTO logs 
             (timestamp, ip, method, path, full_url, status, payload_preview, user_agent, threat_score)
-            VALUES (DATETIME('now'), ?, ?, ?, ?, ?, ?, ?, ?)
+            VALUES (DATETIME('now','+8 hours'), ?, ?, ?, ?, ?, ?, ?, ?)
         """, (ip, method, path, full_url, status, payload_preview, ua, 0))
 
         db.commit()
@@ -88,28 +161,32 @@ def receive_log():
 @app.route("/api/stats/requests")
 def stats_requests():
     db = get_db()
-    rows = db.execute("""
+    where, params = build_filters(request)
+
+    rows = db.execute(f"""
         SELECT strftime('%H:%M', timestamp) AS minute, COUNT(*) AS total
         FROM logs
+        {where}
         GROUP BY minute
-        ORDER BY minute DESC
-        LIMIT 20
-    """).fetchall()
+        ORDER BY minute
+    """, params).fetchall()
 
     return jsonify({
-        "labels": [r["minute"] for r in rows][::-1],
-        "values": [r["total"] for r in rows][::-1]
+        "labels": [r["minute"] for r in rows],
+        "values": [r["total"] for r in rows]
     })
-
 
 @app.route("/api/stats/methods")
 def stats_methods():
     db = get_db()
-    rows = db.execute("""
+    where, params = build_filters(request)
+
+    rows = db.execute(f"""
         SELECT method, COUNT(*) AS total
         FROM logs
+        {where}
         GROUP BY method
-    """).fetchall()
+    """, params).fetchall()
 
     return jsonify({
         "labels": [r["method"] for r in rows],
@@ -119,17 +196,19 @@ def stats_methods():
 @app.route("/api/stats/status")
 def stats_status():
     db = get_db()
-    rows = db.execute("""
+    where, params = build_filters(request)
+
+    rows = db.execute(f"""
         SELECT status, COUNT(*) AS total
         FROM logs
+        {where}
         GROUP BY status
-    """).fetchall()
+    """, params).fetchall()
 
     return jsonify({
         "labels": [str(r["status"]) for r in rows],
         "values": [r["total"] for r in rows]
     })
-
 
 # ============================
 # Run Server
--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,18 +1,54 @@
+// ======================
+// UTIL FUNCTIONS
+// ======================
+
+function threatBadge(score) {
+    if (score >= 70) return "bg-danger";
+    if (score >= 40) return "bg-warning text-dark";
+    return "bg-success";
+}
+
+function isSuspiciousPayload(payload) {
+    if (!payload) return false;
+
+    const patterns = [
+        /or\s+1=1/i,
+        /union\s+select/i,
+        /<script.*?>/i,
+        /--/,
+        /\/\.\./,
+        /sleep\(/i,
+        /benchmark\(/i
+    ];
+
+    return patterns.some(p => p.test(payload));
+}
+
+function truncate(text, max = 40) {
+    if (!text) return "";
+    return text.length > max ? text.substring(0, max) + "..." : text;
+}
+
+// ======================
+// LOAD LOGS
+// ======================
+
 function loadLogs() {
-    fetch("/api/logs")
+    const params = new URLSearchParams(getFilterParams());
+
+    fetch("/api/logs?" + params.toString())
         .then(res => res.json())
         .then(data => {
             const tbody = document.querySelector("#logTable tbody");
             tbody.innerHTML = "";
 
             data.logs.forEach(log => {
+
+                // 🔥 PENTING: harus DI SINI
+                const suspicious = isSuspiciousPayload(log.payload);
+                const preview = truncate(log.payload);
+
                 const tr = document.createElement("tr");
-
-                const threatColor =
-                    log.threat_score >= 80 ? "danger" :
-                    log.threat_score >= 50 ? "warning" : "success";
-
-                const hasPayload = log.payload && log.payload.trim().length > 0;
 
                 tr.innerHTML = `
                     <td>${log.id}</td>
@@ -23,52 +59,45 @@ function loadLogs() {
                     <td>${log.path}</td>
 
                     <td>
-                        <a href="${log.full_url}" target="_blank">${log.full_url}</a>
+                        <a href="${log.full_url}" target="_blank">
+                            ${log.full_url}
+                        </a>
                     </td>
 
                     <td>
-                        <span class="badge bg-info text-dark" data-bs-toggle="tooltip" title="${log.user_agent}">
+                        <span class="badge bg-info text-dark"
+                            style="cursor:pointer"
+                            onclick='showUserAgent(${JSON.stringify(log.user_agent)})'>
                             UA
                         </span>
                     </td>
 
-                    <td class="position-relative">
-                        <button class="btn btn-sm btn-outline-primary position-relative"
-                            onclick='showPayload(${JSON.stringify(log.payload).replace(/'/g, "&apos;")})'>
-                            View
-                        </button>
-
-                        ${hasPayload ? `
-                            <span class="position-absolute top-0 start-100 translate-middle p-1 bg-danger rounded-circle"
-                                style="transform: translate(-85%, 35%);">
-                            </span>` : ""}
+                    <td>
+                        <span class="badge ${suspicious ? "bg-danger" : "bg-secondary"}"
+                            style="cursor:pointer"
+                            onclick='showPayload(${JSON.stringify(log.payload)})'>
+                            ${preview || "empty"}
+                        </span>
                     </td>
 
                     <td>
-                        <span class="badge bg-${threatColor}">
+                        <span class="badge ${threatBadge(log.threat_score)}">
                             ${log.threat_score}
                         </span>
                     </td>
                 `;
 
 
-
                 tbody.appendChild(tr);
             });
-
-            // aktifkan tooltip Bootstrap
-            const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
-            [...tooltipTriggerList].map(el => new bootstrap.Tooltip(el));
-        });
+        })
+        .catch(err => console.error("Load logs error:", err));
 }
 
-setInterval(loadLogs, 3000);
-loadLogs();
-
-
 // ======================
-// PAYLOAD POPUP MODAL
+// PAYLOAD MODAL
 // ======================
+
 function showPayload(payload) {
     const modalBody = document.getElementById("payloadModalBody");
     modalBody.textContent = payload || "(empty)";
@@ -76,16 +105,50 @@ function showPayload(payload) {
     modal.show();
 }
 
-setInterval(loadLogs, 3000);
-loadLogs();
+function showResponse(resp) {
+    document.getElementById("responseModalBody").textContent =
+        resp || "(empty)";
+    new bootstrap.Modal(
+        document.getElementById("responseModal")
+    ).show();
+}
 
+// ======================
+// USER AGENT MODAL
+// ======================
+
+function showUserAgent(ua) {
+    const modalBody = document.getElementById("uaModalBody");
+    modalBody.textContent = ua || "(empty)";
+    const modal = new bootstrap.Modal(document.getElementById("uaModal"));
+    modal.show();
+}
+
+// ======================
+// FILTER PARAMS
+// ======================
+
+function getFilterParams() {
+    return {
+        ip: document.getElementById("filterIp").value,
+        method: document.getElementById("filterMethod").value,
+        status: document.getElementById("filterStatus").value,
+        start: document.getElementById("filterStart").value,
+        end: document.getElementById("filterEnd").value
+    };
+}
+
+// ======================
+// CHARTS
+// ======================
 
 let chartRequests, chartMethods, chartStatus;
 
 function loadCharts() {
+    const params = new URLSearchParams(getFilterParams()).toString();
 
-    // ===== Requests per minute =====
-    fetch("/api/stats/requests")
+    // Requests per minute
+    fetch("/api/stats/requests?" + params)
         .then(res => res.json())
         .then(data => {
             if (chartRequests) chartRequests.destroy();
@@ -102,8 +165,8 @@ function loadCharts() {
             });
         });
 
-    // ===== Methods =====
-    fetch("/api/stats/methods")
+    // Methods
+    fetch("/api/stats/methods?" + params)
         .then(res => res.json())
         .then(data => {
             if (chartMethods) chartMethods.destroy();
@@ -120,8 +183,8 @@ function loadCharts() {
             });
         });
 
-    // ===== Status Codes =====
-    fetch("/api/stats/status")
+    // Status codes
+    fetch("/api/stats/status?" + params)
         .then(res => res.json())
         .then(data => {
             if (chartStatus) chartStatus.destroy();
@@ -139,5 +202,12 @@ function loadCharts() {
         });
 }
 
-setInterval(loadCharts, 5000);
+// ======================
+// INIT
+// ======================
+
+loadLogs();
 loadCharts();
+setInterval(loadCharts, 5000);
+
+// dimana saya bisa melihat response nya, apa saya buat lagi fitur yaa?? sebenarnnya dari dulu saya ingin membuat beberapa fitur saya inginn pnya side bar, tapi saya bingnung apa yang mau saay tambahkan sekranga saya bepikir untuk menambhakan full monitoring yang menampilkan request dan response, ip yang di blokir dll,  menurutmnu apa lagi yang perlu saya masukkan??  
